3dfa9df971fbbdffa08044bc44747f914c97c933
kornelius <ditaditya540@gmail.com>
2023-12-10 23:03:59 +0700

    addpendaftaran

--- resources/views/pendaftaran/addpendaftaran.blade.php
+++ resources/views/pendaftaran/addpendaftaran.blade.php
@@ -11,12 +11,12 @@
                     <div class="card-body">
                         @csrf
                         @include('alerts.success')
-                        <div class="form-group{{ $errors->has('nama') ? ' has-danger' : '' }}">
+                        <div class="form-group{{ $errors->has('namapasien') ? ' has-danger' : '' }}">
                             <label>{{ __('Nama Pasien') }}</label>
-                            <input type="text" name="nama" class="form-control{{ $errors->has('nama') ? ' is-invalid' : '' }}" placeholder="{{ __('Nama') }}" value="{{ old('nama') }}">
-                            @include('alerts.feedback', ['field' => 'nama'])
+                            <input type="text" name="namapasien" class="form-control{{ $errors->has('namapasien') ? ' is-invalid' : '' }}" placeholder="{{ __('Namapasien') }}" value="{{ old('namapasien') }}">
+                            @include('alerts.feedback', ['field' => 'namapasien'])
                         </div>
-                        <div class="form-group{{ $errors->has('poli') ? ' has-danger' : '' }}">
+                        <!-- <div class="form-group{{ $errors->has('poli') ? ' has-danger' : '' }}">
                             <label>{{ __('Poli') }}</label>
                             <select name="poli" class="form-control{{ $errors->has('poli') ? ' is-invalid' : '' }}">
                                 <option style="color: black;" value="" selected disabled>{{ __('Pilih Poli') }}</option>
@@ -27,7 +27,7 @@
                                 <option style="color: black;" value="Splesialis kandungan 2" {{ old('poli') == 'Splesialis kandungan 2' ? 'selected' : '' }}>{{ __('Splesialis kandungan 2') }}</option>
                             </select>
                             @include('alerts.feedback', ['field' => 'poli'])
-                        </div>
+                        </div> -->
                         <div class="form-group{{ $errors->has('tanggaldaftar') ? ' has-danger' : '' }}">
                             <label>{{ __('Tanggal daftar') }}</label>
                             <input type="date" name="tanggaldaftar" class="form-control{{ $errors->has('tanggaldaftar') ? ' is-invalid' : '' }}" value="{{ old('tanggaldaftar') }}">
@@ -35,13 +35,46 @@
                         </div>
                         <div class="form-group{{ $errors->has('jadwal') ? ' has-danger' : '' }}">
                             <label>{{ __('Jadwal Dokter') }}</label>
-                            <select name="jadwal" class="form-control{{ $errors->has('jadwal') ? ' is-invalid' : '' }}">
+                            <select name="Senin" class="form-control{{ $errors->has('Senin') ? ' is-invalid' : '' }}">
                                 <option style="color: black;" value="" selected disabled>{{ __('Pilih Jadwal') }}</option>
-                                <option style="color: black;" value="08.00-09.30" {{ old('jadwal') == '08.00-09.30' ? 'selected' : '' }}>{{ __('08.00-09.30') }}</option>
-                                <option style="color: black;" value="08.00-10.00" {{ old('jadwal') == '08.00-10.00' ? 'selected' : '' }}>{{ __('08.00-10.00') }}</option>
-                                <option style="color: black;" value="10.00-15.00" {{ old('jadwal') == '10.00-15.00' ? 'selected' : '' }}>{{ __('10.00-15.00') }}</option>
-                                <option style="color: black;" value="12.00-14.00" {{ old('jadwal') == '12.00-14.00' ? 'selected' : '' }}>{{ __('12.00-14.00') }}</option>
-                                <option style="color: black;" value="14.00-16.00" {{ old('jadwal') == '14.00-16.00' ? 'selected' : '' }}>{{ __('14.00-16.00') }}</option>
+                                <option style="color: black;" value="Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00" {{ old('jadwal') == 'Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00' ? 'selected' : '' }}>{{ __('Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00') }}</option>
+                                <option style="color: black;" value="Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00" {{ old('jadwal') == 'Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00' ? 'selected' : '' }}>{{ __('Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00') }}</option>
+                                <option style="color: black;" value="Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30" {{ old('jadwal') == 'Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30' ? 'selected' : '' }}>{{ __('Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30') }}</option>
+                                <option style="color: black;" value="Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00" {{ old('jadwal') == 'Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00' ? 'selected' : '' }}>{{ __('Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00') }}</option>
+                            </select>
+                            <select name="Selasa" class="form-control{{ $errors->has('Selasa') ? ' is-invalid' : '' }}">
+                                <option style="color: black;" value="" selected disabled>{{ __('Pilih Jadwal') }}</option>
+                                <option style="color: black;" value="Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00" {{ old('jadwal') == 'Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00' ? 'selected' : '' }}>{{ __('Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00') }}</option>
+                                <option style="color: black;" value="Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00" {{ old('jadwal') == 'Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00' ? 'selected' : '' }}>{{ __('Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00') }}</option>
+                                <option style="color: black;" value="Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30" {{ old('jadwal') == 'Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30' ? 'selected' : '' }}>{{ __('Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30') }}</option>
+                                <option style="color: black;" value="Splesialis Kandungan | dr. Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00" {{ old('jadwal') == 'Splesialis Kandungan | dr. Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00' ? 'selected' : '' }}>{{ __('Splesialis Kandungan | dr. Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00') }}</option>
+                            </select>
+                            <select name="Rabu" class="form-control{{ $errors->has('Rabu') ? ' is-invalid' : '' }}">
+                                <option style="color: black;" value="" selected disabled>{{ __('Pilih Jadwal') }}</option>
+                                <option style="color: black;" value="Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00" {{ old('jadwal') == 'Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00' ? 'selected' : '' }}>{{ __('Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00') }}</option>
+                                <option style="color: black;" value="Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00" {{ old('jadwal') == 'Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00' ? 'selected' : '' }}>{{ __('Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00') }}</option>
+                                <option style="color: black;" value="Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30" {{ old('jadwal') == 'Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30' ? 'selected' : '' }}>{{ __('Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30') }}</option>
+                                <option style="color: black;" value="Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00" {{ old('jadwal') == 'Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00' ? 'selected' : '' }}>{{ __('Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00') }}</option>
+                            </select>
+                            <select name="Kamis" class="form-control{{ $errors->has('Kamis') ? ' is-invalid' : '' }}">
+                                <option style="color: black;" value="" selected disabled>{{ __('Pilih Jadwal') }}</option>
+                                <option style="color: black;" value="Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00" {{ old('jadwal') == 'Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00' ? 'selected' : '' }}>{{ __('Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00') }}</option>
+                                <option style="color: black;" value="Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00" {{ old('jadwal') == 'Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00' ? 'selected' : '' }}>{{ __('Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00') }}</option>
+                                <option style="color: black;" value="Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30" {{ old('jadwal') == 'Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30' ? 'selected' : '' }}>{{ __('Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30') }}</option>
+                                <option style="color: black;" value="Splesialis Kandungan | dr.  Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00" {{ old('jadwal') == 'Splesialis Kandungan | dr.  Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00' ? 'selected' : '' }}>{{ __('Splesialis Kandungan | dr.  Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00') }}</option>
+                            </select>
+                            <select name="Jumat" class="form-control{{ $errors->has('Jumat') ? ' is-invalid' : '' }}">
+                                <option style="color: black;" value="" selected disabled>{{ __('Pilih Jadwal') }}</option>
+                                <option style="color: black;" value="Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00" {{ old('jadwal') == 'Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00' ? 'selected' : '' }}>{{ __('Spesialis Anak | dr. Hans Angelius suharto, Sp. A | 14.00-16.00') }}</option>
+                                <option style="color: black;" value="Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00" {{ old('jadwal') == 'Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00' ? 'selected' : '' }}>{{ __('Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 12.00-14.00') }}</option>
+                                <option style="color: black;" value="Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30" {{ old('jadwal') == 'Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30' ? 'selected' : '' }}>{{ __('Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30') }}</option>
+                                <option style="color: black;" value="Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00" {{ old('jadwal') == 'Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00' ? 'selected' : '' }}>{{ __('Splesialis Kandungan | dr. Hariyati Wijaya, Sp.OG | 10.00-15.00') }}</option>
+                            </select>
+                            <select name="Sabtu" class="form-control{{ $errors->has('Sabtu') ? ' is-invalid' : '' }}">
+                                <option style="color: black;" value="" selected disabled>{{ __('Pilih Jadwal') }}</option>
+                                <option style="color: black;" value="Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00" {{ old('jadwal') == 'Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00' ? 'selected' : '' }}>{{ __('Splesialis Bedah | dr. Billy Jeffkien A. ohe, Sp.B | 08.00-10.00') }}</option>
+                                <option style="color: black;" value="Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30" {{ old('jadwal') == 'Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30' ? 'selected' : '' }}>{{ __('Splesialis Gizi Klinik | dr. Aprinand Tandoek, M.Kes. Sp.GK | 08.00-09.30') }}</option>
+                                <option style="color: black;" value="Splesialis Kandungan | dr. Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00" {{ old('jadwal') == 'Splesialis Kandungan | dr. Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00' ? 'selected' : '' }}>{{ __('Splesialis Kandungan | dr. Arthur Todingbua, Sp.OG, M.Kes | 10.00-15.00') }}</option>
                             </select>
                             @include('alerts.feedback', ['field' => 'jadwal'])
                         </div>
@@ -63,11 +96,6 @@
     </div>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            var notelpInput = document.querySelector('input[name="notelp"]');
-            notelpInput.addEventListener('input', function () {
-                notelpInput.value = notelpInput.value.replace(/[^0-9+]/g, '');
-            });
-        });
+
     </script>
 @endsection
